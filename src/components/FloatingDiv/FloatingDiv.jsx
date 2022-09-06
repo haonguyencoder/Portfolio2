9e6247ca9cd77195355e6b12f1349f23c46502d5
Ruddy <honghao.2107@gmail.com>
2022-09-06 10:52:41 +0700

    first commit

---
+++ src/components/FloatingDiv/FloatingDiv.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+import './FloatingDiv.css'
+
+
+const FloatingDiv = ({img, text1, text2}) => {
+  return (
+    // darkMode
+    <div className="floatingDiv">
+      <img src={img} alt="" />
+      <span>
+        {text1}
+        <br/>
+        {text2}
+      </span>
+    </div>
+  );
+};
+
+export default FloatingDiv;
